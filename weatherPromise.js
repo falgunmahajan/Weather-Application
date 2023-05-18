c39b2dae5c2630efd71a3bc2db5668b87b09cd92
falgunmahajan <falgunmahajan1999@gmail.com>
2023-05-18 23:56:00 +0530

    updated with async/await

--- weatherPromise.js
+++ weatherPromise.js
@@ -1,40 +1,33 @@
 const url = `https://api.openweathermap.org/data/2.5/weather`;
-const apiKey ="d42251a0a9d8f7e6b274dd1ecf3c1edc";
+const apiKey = "d42251a0a9d8f7e6b274dd1ecf3c1edc";
 const searchBtn = document.querySelector("#btn");
 
-function getData(data)
-{
-    document.querySelector(".error").style.display="none";
-    document.querySelector(".info").style.display="block";
-    document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temp").innerHTML=data.main.temp+"°C";
-    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-    document.querySelector(".Windspeed").innerHTML=data.wind.speed+" km/h";
+function getData(data) {
+    if (data.name == undefined) {
+        getError(data.message)
+    }
+    else {
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".info").style.display = "block";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".Windspeed").innerHTML = data.wind.speed + " km/h";
+    }
 }
-function getError(err)
-{
-    document.querySelector(".error").style.display="block";
+function getError(err) {
+    document.querySelector(".error").style.display = "block";
 
-    document.querySelector(".error").innerHTML=err;
-    document.querySelector(".info").style.display="none"
-    
+    document.querySelector(".error").innerHTML = err;
+    document.querySelector(".info").style.display = "none"
+
 }
-function getCity(city)
-{
-    const promise = new Promise((resolve,reject)=>{
-    const data = fetch(`${url}?q=${city}&appid=${apiKey}&units=metric`)
-    .then((data)=>{
-        if(data.status===200)
-        {
-            resolve(data);
-        }
-        reject("City not found");
-    })
-    })
-    promise.then((data)=>data.json()).then((data)=>getData(data)).catch((err)=>getError(err))
+async function getCity(city) {
+    const weatherDetails = await fetch(`${url}?q=${city}&appid=${apiKey}&units=metric`);
+    const data = await weatherDetails.json();
+    getData(data);
 }
-searchBtn.addEventListener("click", ()=>
-{
+searchBtn.addEventListener("click", () => {
     const city = document.querySelector("#text").value;
     getCity(city)
 }
